56cd7f4f0084af43a810fcaa4a8d270ac2faa2dc
calebkang <calebkang@SOLCWL0660.sol.st.com>
2023-08-23 10:24:25 +0900

    init

---
+++ Core/Src/app_threadx.c
@@ -0,0 +1,203 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    app_threadx.c
+  * @author  MCD Application Team
+  * @brief   ThreadX applicative file
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2020-2021 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "app_threadx.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "main.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+TX_THREAD MainThread;
+TX_THREAD ThreadOne;
+TX_THREAD ThreadTwo;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+void ThreadOne_Entry(ULONG thread_input);
+void ThreadTwo_Entry(ULONG thread_input);
+void MainThread_Entry(ULONG thread_input);
+void App_Delay(uint32_t Delay);
+/* USER CODE END PFP */
+
+/**
+  * @brief  Application ThreadX Initialization.
+  * @param memory_ptr: memory pointer
+  * @retval int
+  */
+UINT App_ThreadX_Init(VOID *memory_ptr)
+{
+  UINT ret = TX_SUCCESS;
+  /* USER CODE BEGIN App_ThreadX_MEM_POOL */
+  TX_BYTE_POOL *byte_pool = (TX_BYTE_POOL*)memory_ptr;
+  /* USER CODE END App_ThreadX_MEM_POOL */
+
+  /* USER CODE BEGIN App_ThreadX_Init */
+  CHAR *pointer;
+
+  /* Allocate the stack for MainThread.  */
+  if (tx_byte_allocate(byte_pool, (VOID **) &pointer,
+                       APP_STACK_SIZE, TX_NO_WAIT) != TX_SUCCESS)
+  {
+    ret = TX_POOL_ERROR;
+  }
+
+  /* Create MainThread.  */
+  if (tx_thread_create(&MainThread, "Main Thread", MainThread_Entry, 0,
+                       pointer, APP_STACK_SIZE,
+                       MAIN_THREAD_PRIO, MAIN_THREAD_PREEMPTION_THRESHOLD,
+                       TX_NO_TIME_SLICE, TX_AUTO_START) != TX_SUCCESS)
+  {
+    ret = TX_THREAD_ERROR;
+  }
+
+  /* Allocate the stack for ThreadOne.  */
+  if (tx_byte_allocate(byte_pool, (VOID **) &pointer,
+                       APP_STACK_SIZE, TX_NO_WAIT) != TX_SUCCESS)
+  {
+    ret = TX_POOL_ERROR;
+  }
+
+  /* Create ThreadOne.  */
+  if (tx_thread_create(&ThreadOne, "Thread One", ThreadOne_Entry, 0,
+                       pointer, APP_STACK_SIZE,
+                       THREAD_ONE_PRIO, THREAD_ONE_PREEMPTION_THRESHOLD,
+                       TX_NO_TIME_SLICE, TX_AUTO_START) != TX_SUCCESS)
+  {
+    ret = TX_THREAD_ERROR;
+  }
+
+  /* Allocate the stack for ThreadTwo.  */
+  if (tx_byte_allocate(byte_pool, (VOID **) &pointer,
+                       APP_STACK_SIZE, TX_NO_WAIT) != TX_SUCCESS)
+  {
+    ret = TX_POOL_ERROR;
+  }
+
+  /* Create ThreadTwo.  */
+  if (tx_thread_create(&ThreadTwo, "Thread Two", ThreadTwo_Entry, 0,
+                       pointer, APP_STACK_SIZE,
+                       THREAD_TWO_PRIO, THREAD_TWO_PREEMPTION_THRESHOLD,
+                       TX_NO_TIME_SLICE, TX_AUTO_START) != TX_SUCCESS)
+  {
+    ret = TX_THREAD_ERROR;
+  }
+  /* USER CODE END App_ThreadX_Init */
+
+  return ret;
+}
+
+  /**
+  * @brief  Function that implements the kernel's initialization.
+  * @param  None
+  * @retval None
+  */
+void MX_ThreadX_Init(void)
+{
+  /* USER CODE BEGIN  Before_Kernel_Start */
+
+  /* USER CODE END  Before_Kernel_Start */
+
+  tx_kernel_enter();
+
+  /* USER CODE BEGIN  Kernel_Start_Error */
+
+  /* USER CODE END  Kernel_Start_Error */
+}
+
+/* USER CODE BEGIN 1 */
+/**
+  * @brief  Function implementing the MainThread thread.
+  * @param  thread_input: Not used
+  * @retval None
+  */
+void MainThread_Entry(ULONG thread_input)
+{
+  /* Infinite loop */
+  while(1)
+  {
+    HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+    /* Thread sleep for 100ms */
+    tx_thread_sleep(100);
+  }
+}
+
+/**
+  * @brief  Function implementing the ThreadOne thread.
+  * @param  thread_input: Not used
+  * @retval None
+  */
+void ThreadOne_Entry(ULONG thread_input)
+{
+  /* Infinite loop */
+  while(1)
+  {
+    HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+    /* Thread sleep for 500ms */
+    tx_thread_sleep(500);
+  }
+}
+
+/**
+  * @brief  Function implementing the ThreadTwo thread.
+  * @param  thread_input: Not used
+  * @retval None
+  */
+void ThreadTwo_Entry(ULONG thread_input)
+{
+  /* Infinite loop */
+  while(1)
+  {
+    HAL_GPIO_TogglePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin);
+    /* Thread sleep for 1s */
+    tx_thread_sleep(1000);
+  }
+}
+
+/**
+  * @brief  Application Delay function.
+  * @param  Delay : number of ticks to wait
+  * @retval None
+  */
+void App_Delay(uint32_t Delay)
+{
+  UINT initial_time = tx_time_get();
+  while ((tx_time_get() - initial_time) < Delay);
+}
+/* USER CODE END 1 */
